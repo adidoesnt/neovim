f1b1129b3c99ac891dc490d102b919bc01aa6ffc
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-03-13 14:43:42 +0800

    update plugin configs

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,20 +2,18 @@ return	{
 				"zbirenbaum/copilot.lua",
 				cmd = "Copilot",
 				event = "InsertEnter",
-				config = function() 
-								require("copilot").setup({
-												suggestion = {
-																enabled = true,
-																auto_trigger = true,
-																keymap = {
-																				accept = "<C-a>",
-																				replace = "<C-r>",
-																				next = "<C-n>",
-																				prev = "<C-p>",
-																				ignore = "<C-i>",
-																				cancel = "<C-c>",	
-																}
+				config = {
+								suggestion = {
+												enabled = true,
+												auto_trigger = true,
+												keymap = {
+																accept = "<C-a>",
+																replace = "<C-r>",
+																next = "<C-n>",
+																prev = "<C-p>",
+																ignore = "<C-i>",
+																cancel = "<C-c>",	
 												}
-								})
-				end
+								}
+				}
 }
--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,16 +1,16 @@
-return	{
+return {
 				"kdheepak/lazygit.nvim",
-				cmd = {
-								"LazyGit",
-								"LazyGitConfig",
-								"LazyGitCurrentFile",
-								"LazyGitFilter",
-								"LazyGitFilterCurrentFile",
-				},
-				dependencies = {
-								"nvim-lua/plenary.nvim",
-				},
-				config = function()
-								vim.keymap.set('n', '<Leader>lg', ':LazyGit<CR>', { noremap = true }) -- install lazygit via homebrew for this to work
-				end
+								cmd = {
+												"LazyGit",
+												"LazyGitConfig",
+												"LazyGitCurrentFile",
+												"LazyGitFilter",
+												"LazyGitFilterCurrentFile",
+								},
+								dependencies = {
+												"nvim-lua/plenary.nvim",
+								},
+								config = function()
+												vim.keymap.set('n', '<Leader>lg', ':LazyGit<CR>', { noremap = true })
+								end
 }
