80534e39af8a331898d6d5ea6e06423b9583f84b
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-03-13 10:53:57 +0800

    shift nightfly to plugins folder

--- init.lua
+++ init.lua
@@ -5,56 +5,20 @@ vim.wo.number = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+				vim.fn.system({
+								"git",
+								"clone",
+								"--filter=blob:none",
+								"https://github.com/folke/lazy.nvim.git",
+								"--branch=stable", -- latest stable release
+								lazypath,
+				})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-				{ "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-				{
-								'nvim-telescope/telescope.nvim', tag = '0.1.5',
-								dependencies = {'nvim-lua/plenary.nvim'}
-				},
-				{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-				{
-								"nvim-neo-tree/neo-tree.nvim",
-								branch = "v3.x",
-								dependencies = {
-												"nvim-lua/plenary.nvim",
-												"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-												"MunifTanjim/nui.nvim",
-								}
-				},
-				{
-								"kdheepak/lazygit.nvim",
-								cmd = {
-												"LazyGit",
-												"LazyGitConfig",
-												"LazyGitCurrentFile",
-												"LazyGitFilter",
-												"LazyGitFilterCurrentFile",
-								},
-								dependencies = {
-												"nvim-lua/plenary.nvim",
-								}
-				},
-				{
-								"zbirenbaum/copilot.lua",
-								cmd = "Copilot",
-								event = "InsertEnter",
-				},
-}
-
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 require("copilot").setup({
 				suggestion = {
---
+++ lua/plugins.lua
@@ -0,0 +1,34 @@
+return {
+				{
+								'nvim-telescope/telescope.nvim', tag = '0.1.5',
+								dependencies = {'nvim-lua/plenary.nvim'}
+				},
+				{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+				{
+								"nvim-neo-tree/neo-tree.nvim",
+								branch = "v3.x",
+								dependencies = {
+												"nvim-lua/plenary.nvim",
+												"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+												"MunifTanjim/nui.nvim",
+								}
+				},
+				{
+								"kdheepak/lazygit.nvim",
+								cmd = {
+												"LazyGit",
+												"LazyGitConfig",
+												"LazyGitCurrentFile",
+												"LazyGitFilter",
+												"LazyGitFilterCurrentFile",
+								},
+								dependencies = {
+												"nvim-lua/plenary.nvim",
+								}
+				},
+				{
+								"zbirenbaum/copilot.lua",
+								cmd = "Copilot",
+								event = "InsertEnter",
+				},
+}
